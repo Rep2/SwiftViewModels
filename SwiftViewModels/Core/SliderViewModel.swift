a73433090c1ff3763c983d2f5d18da1a3ed2a325
Ivan Rep <ivan.rep2@gmail.com>
2018-04-12 21:16:25 +0200

    SliderViewModel

---
+++ SwiftViewModels/Core/SliderViewModel.swift
@@ -0,0 +1,24 @@
+import UIKit
+
+public struct SliderViewModel {
+    public let minimum: Float
+    public let maximum: Float
+    public let current: Float
+    public let viewViewModel: ViewViewModel
+
+    public init(minimum: Float, maximum: Float, current: Float, viewViewModel: ViewViewModel) {
+        self.minimum = minimum
+        self.maximum = maximum
+        self.current = current
+
+        self.viewViewModel = viewViewModel
+    }
+
+    public func apply(toSlider slider: UISlider) {
+        slider.minimumValue = minimum
+        slider.maximumValue = maximum
+        slider.value = current
+
+        viewViewModel.apply(toView: slider)
+    }
+}
