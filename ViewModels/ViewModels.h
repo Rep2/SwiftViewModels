4e163ae40be81fc810793193fc3c36dc8c09f9de
Ivan Rep <ivan.rep2@gmail.com>
2018-04-12 16:26:33 +0200

    Initial commit

---
+++ ViewModels/ViewModels.h
@@ -0,0 +1,19 @@
+//
+//  ViewModels.h
+//  ViewModels
+//
+//  Created by Ivan Rep on 12/04/2018.
+//  Copyright © 2018 Rep. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ViewModels.
+FOUNDATION_EXPORT double ViewModelsVersionNumber;
+
+//! Project version string for ViewModels.
+FOUNDATION_EXPORT const unsigned char ViewModelsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ViewModels/PublicHeader.h>
+
+
