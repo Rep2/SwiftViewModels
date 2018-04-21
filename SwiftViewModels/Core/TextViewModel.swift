b256636371211fe2790d7c7704a91bd5f6030cf4
Ivan Rep <ivan.rep2@gmail.com>
2018-04-21 12:59:52 +0200

    Gitignore and swiftlint

--- SwiftViewModels/Core/TextViewModel.swift
+++ SwiftViewModels/Core/TextViewModel.swift
@@ -4,11 +4,13 @@ public struct TextViewModel {
     public var text: String?
     public let textColor: UIColor
     public let font: UIFont
+    public let viewViewModel: ViewViewModel
 
-    public init(text: String?, textColor: UIColor = .black, font: UIFont = UIFont.systemFont(ofSize: 15)) {
+    public init(text: String?, textColor: UIColor = .black, font: UIFont = UIFont.systemFont(ofSize: 15), viewViewModel: ViewViewModel) {
         self.text = text
         self.textColor = textColor
         self.font = font
+        self.viewViewModel = viewViewModel
     }
 
     public func apply(toLabel label: UILabel) {
@@ -23,5 +25,7 @@ public struct TextViewModel {
         if label.font != font {
             label.font = font
         }
+
+        viewViewModel.apply(toView: label)
     }
 }
