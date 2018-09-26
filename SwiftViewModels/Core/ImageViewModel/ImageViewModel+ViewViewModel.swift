d80cdc5fe4de334f70e7b5f642381b23a5b4c1af
Ivan Rep <ivan.rep2@gmail.com>
2018-09-26 03:11:53 +0200

    Generic imageViewModel

--- SwiftViewModels/Core/ImageViewModel/ImageViewModel+ViewViewModel.swift
+++ SwiftViewModels/Core/ImageViewModel/ImageViewModel+ViewViewModel.swift
@@ -1,9 +1,11 @@
 import UIKit
 
-extension Array: ImageViewModel where Element: ImageViewModel {
+extension Array: ImageViewModel {
     public func apply(to imageView: UIImageView) {
         self.forEach {
-            $0.apply(to: imageView)
+            guard let viewModel = $0 as? ImageViewModel else { return }
+
+            viewModel.apply(to: imageView)
         }
     }
 }
