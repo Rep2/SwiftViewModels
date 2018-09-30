5ab9793c5df94b7d94cff50b3c9e10e82a7d10c6
Ivan Rep <ivan.rep2@gmail.com>
2018-09-30 15:22:57 +0200

    Slider images

--- SwiftViewModels/BasicUI/SliderViewModel.swift
+++ SwiftViewModels/BasicUI/SliderViewModel.swift
@@ -6,12 +6,18 @@ public struct SliderViewModel {
     public let current: Float
     public let viewViewModel: ViewViewModel?
 
-    public init(minimum: Float, maximum: Float, current: Float, viewViewModel: ViewViewModel? = nil) {
+    public let leftImage: UIImage?
+    public let rightImage: UIImage?
+
+    public init(minimum: Float, maximum: Float, current: Float, viewViewModel: ViewViewModel? = nil, leftImage: UIImage? = nil, rightImage: UIImage? = nil) {
         self.minimum = minimum
         self.maximum = maximum
         self.current = current
 
         self.viewViewModel = viewViewModel
+
+        self.leftImage = leftImage
+        self.rightImage = rightImage
     }
 
     public func apply(toSlider slider: UISlider) {
@@ -20,5 +26,8 @@ public struct SliderViewModel {
         slider.value = current
 
         viewViewModel?.apply(to: slider)
+
+        slider.minimumValueImage = leftImage
+        slider.maximumValueImage = rightImage
     }
 }
